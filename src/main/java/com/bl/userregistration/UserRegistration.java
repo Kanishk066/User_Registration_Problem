c3ed6cbf14b8d44af4276c3e151e5d21d6303efa
Kanishk <kanishksinghal06@gmail.com>
2023-05-05 09:04:22 +0530

    Added the code for valid password

--- src/main/java/com/bl/userregistration/UserRegistration.java
+++ src/main/java/com/bl/userregistration/UserRegistration.java
@@ -3,53 +3,62 @@ package com.bl.userregistration;
 import java.util.Scanner;
 
 public class UserRegistration {
+    public static boolean isValidFirstName(String firstName) {
+        return firstName.matches("[A-Z][a-zA-Z]{2,}");
+    }
+    public static boolean isValidLastName(String lastName) {
+        return lastName.matches("[A-Z][a-zA-Z]{2,}");
+    }
+    public static boolean isValidEmail(String email) {
+        return email.matches("[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.][a-zA-Z]{2,}){1,2}");
+    }
+    public static boolean isValidMobileNumber(String mobileNumber) {
+        return mobileNumber.matches("\\d{2} \\d{10}");
+    }
+    public static boolean isValidPassword(String password) {
+        return password.length() >= 8;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        getValidFirstName(sc);
-        getValidLastName(sc);
-        getValidEmail(sc);
-        getValidMobileNumber(sc);
-        System.out.println("Valid first name, last name, email, and mobile number!");
-    }
-    public static String getValidFirstName(Scanner sc) {
-        System.out.println("Enter your first  name: ");
+        System.out.println("Enter your first name: ");
         String firstName = sc.nextLine();
-        boolean isValidFirstName = firstName.matches("[A-Z][a-zA-Z]{2,}");
-        if (!isValidFirstName) {
-            System.out.println("Invalid First Name");
-            firstName = getValidFirstName(sc);
-        }
-        return firstName;
-    }
-    public static String getValidLastName(Scanner sc) {
-        System.out.println("Enter your last  name: ");
+        boolean isValidFirstName = isValidFirstName(firstName);
+
+        System.out.println("Enter your last name: ");
         String lastName = sc.nextLine();
-        boolean isValidLastName = lastName.matches("[A-Z][a-zA-Z]{2,}");
-        if (!isValidLastName) {
-            System.out.println("Invalid First Name");
-            lastName = getValidLastName(sc);
-        }
-        return lastName;
-    }
-    public static String getValidEmail(Scanner sc) {
-        System.out.println("Enter your email id: ");
+        boolean isValidLastName = isValidLastName(lastName);
+
+        System.out.println("Enter your email: ");
         String email = sc.nextLine();
-        boolean isValidEmail = email.matches("[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.][a-zA-Z]{2,}){1,2}");
-        if (!isValidEmail) {
-            System.out.println("Invalid Email Id");
-            email = getValidEmail(sc);
-        }
-        return email;
-    }
-    public static String getValidMobileNumber(Scanner sc){
+        boolean isValidEmail = isValidEmail(email);
+
         System.out.println("Enter your mobile number: ");
         String mobileNumber = sc.nextLine();
-        boolean isValidMobileNumber = mobileNumber.matches("[0-9]{2} [0-9]{10}");
-        if (!isValidMobileNumber){
-            System.out.println("Invalid mobile number!");
-            mobileNumber = getValidMobileNumber(sc);
+        boolean isValidMobileNumber = isValidMobileNumber(mobileNumber);
+
+        System.out.println("Enter your password: ");
+        String password = sc.nextLine();
+        boolean isValidPassword = isValidPassword(password);
+
+        if (isValidFirstName && isValidLastName && isValidEmail && isValidMobileNumber && isValidPassword) {
+            System.out.println("Valid first name, last name, email, mobile number, and password!");
+        } else {
+            if (!isValidFirstName) {
+                System.out.println("Invalid first name! Please enter a valid first name");
+            }
+            if (!isValidLastName) {
+                System.out.println("Invalid last name! Please enter a valid last name");
+            }
+            if (!isValidEmail) {
+                System.out.println("Invalid email! Please enter a valid email address");
+            }
+            if (!isValidMobileNumber) {
+                System.out.println("Invalid mobile number! Please enter a valid mobile number");
+            }
+            if (!isValidPassword) {
+                System.out.println("Invalid password! Please enter a valid password");
+            }
         }
-        return mobileNumber;
     }
 }
 
