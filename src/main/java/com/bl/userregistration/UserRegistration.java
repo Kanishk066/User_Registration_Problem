46a1739dd1df8d9d3990102eba2e2f64851f8be6
Kanishk <kanishksinghal06@gmail.com>
2023-05-05 07:45:52 +0530

    Added the code for valid last name

--- src/main/java/com/bl/userregistration/UserRegistration.java
+++ src/main/java/com/bl/userregistration/UserRegistration.java
@@ -6,6 +6,7 @@ public class UserRegistration {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String firstName = getValidFirstName(sc);
+        String lastName = getValidLastName(sc);
     }
     public static String getValidFirstName(Scanner sc) {
         System.out.println("Enter your first  name: ");
@@ -17,4 +18,15 @@ public class UserRegistration {
         }
         return firstName;
     }
+    public static String getValidLastName(Scanner sc){
+        System.out.println("Enter your last  name: ");
+        String lastName = sc.nextLine();
+        boolean isValidLastName = lastName.matches("[A-Z][a-zA-Z]{2,}");
+        if (!isValidLastName) {
+            System.out.println("Invalid First Name");
+            lastName = getValidLastName(sc);
+        }
+        return lastName;
+    }
+
 }
